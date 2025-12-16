29a9ffbb71f5aa1369063f7973d5c4a578699976
erickcolier <erickcolier@gmail.com>
2025-12-16 11:02:13 -0300

    Rename dist to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/SECU_RED/', // 👈 ESTA LÍNEA ES LA CLAVE
+  base: '/SECU_RED/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
